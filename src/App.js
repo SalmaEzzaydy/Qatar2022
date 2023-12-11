3965e5d5b4b51cd8f63873af1cf91a3cf2aaf9ba
salma <salmaezzaydy13@gmail.com>
2023-12-12 00:00:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Translator from './Translator/Translator';
+import Weather from './Weather/Weather';
+import Footer from './footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Translator />
+      <Weather />
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/footer/Footer.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import './footer.css';
+export default function Footer() {
+  return (
+    <footer id="footer" class="footer-1">
+    <div class="main-footer widgets-dark typo-light">
+    <div class="container">
+    <div class="row">
+      
+    <div class="col-xs-12 col-sm-6 col-md-3">
+    <div class="widget subscribe no-box">
+    <h5 class="widget-title">Quatari2022<span></span></h5>
+    <p>View scores & schedule for the Men's FIFA World Cup 2022 at FOX Sports. Dates, times, tv network, team matchups, odds and more. </p>
+    </div>
+    </div>
+    
+    <div class="col-xs-12 col-sm-6 col-md-3">
+    <div class="widget no-box">
+    <h5 class="widget-title">Quick Links<span></span></h5>
+    <ul class="thumbnail-widget">
+    <li>
+    <div class="thumb-content"><a href="#.">Home</a></div> 
+    </li>
+    <li>
+    <div class="thumb-content"><a href="#.">Translation</a></div> 
+    </li>
+    <li>
+    <div class="thumb-content"><a href="#.">Matchs schedule</a></div> 
+    </li>
+    <li>
+    <div class="thumb-content"><a href="#.">Weather</a></div> 
+    </li>
+    <li>
+    <div class="thumb-content"><a href="#.">Contact</a></div>  
+    </li>
+    </ul>
+    </div>
+    </div>
+    
+    <div class="col-xs-12 col-sm-6 col-md-3">
+    <div class="widget no-box">
+    <h5 class="widget-title">Get Started<span></span></h5>
+    <p>Sign up for MailOnline newsletters to get breaking news delivered to your inbox.</p>
+    <a class="btn" href="https://bit.ly/3m9avif" target="_blank">Subscribe Now</a>
+    </div>
+    </div>
+    
+    <div class="col-xs-12 col-sm-6 col-md-3">
+    
+    <div class="widget no-box">
+    <h5 class="widget-title">Contact Us<span></span></h5>
+    
+    <p><a href="mailto:info@domain.com" title="glorythemes">info@qatari2022.com</a></p>
+    <ul class="social-footer2">
+        <li class=""><a title="youtube" target="_blank" href="https://bit.ly/3m9avif">You</a></li>
+        <li class=""><a href="https://www.facebook.com/" target="_blank" title="Facebook">Fb</a></li>
+        <li class=""><a href="https://twitter.com" target="_blank" title="Twitter">Tw</a></li>
+        <li class=""><a title="instagram" target="_blank" href="https://www.instagram.com/">Insta</a></li>
+    </ul>
+    </div>
+    </div>
+    
+    </div>
+    </div>
+    </div>
+      
+    <div class="footer-copyright">
+    <div class="container">
+    <div class="row">
+    <div class="col-md-12 text-center">
+    <p>Copyright qatari2022 © 2022. All rights reserved.</p>
+    </div>
+    </div>
+    </div>
+    </div>
+    </footer>
+
+  )
+}
